40c330012bc3d386ce8e68d8cfc74e8a1cd7d462
JoaoFranco23 <147751659+JoaoFranco23@users.noreply.github.com>
2024-06-13 20:50:59 -0300

    Create save.js

---
+++ save.js
@@ -0,0 +1,6 @@
+function saveCheckbox(){
+    var Email = document.getElementById('Email');
+    var senha = document.getElementById('senha');
+    var Email = document.querySelector('input["Email"]')
+
+}
